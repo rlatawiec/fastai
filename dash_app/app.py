b1da19c1cdc70904f2728a17bc8ac1ac929d97f8
paulina.knut <paulina.knut@codilime.com>
2019-05-04 18:38:09 +0200

    App core

---
+++ dash_app/app.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+
+from app_content import img_tab, model_tab, video_tab
+
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__)  # external_stylesheets=external_stylesheets
+
+app.layout = html.Div(children=[
+    html.Div([
+        html.Span("App - img visualisation", className='app-title')
+    ], className="row header"),
+    
+    html.Div([
+            dcc.Tabs(
+                id="tabs",
+                style={"height":"20","verticalAlign":"middle"},
+                children=[
+                    dcc.Tab(label="Img visualisation", value="img_tab"),
+                    dcc.Tab(label="Model", value="model_tab"),
+                    dcc.Tab(label="Video", value="video_tab"),
+                ],
+                value="img_tab",
+            )
+    ], className="row tabs_div"
+    ),
+    html.Div(id="tab_content", className="row", style={"margin": "2% 3%"}),
+    
+])
+
+@app.callback(Output("tab_content", "children"), [Input("tabs", "value")])
+def render_content(tab):
+    if tab == "img_tab":
+        return img_tab()
+    elif tab == "model_tab":
+        return model_tab()
+    elif tab == "video_tab":
+        return video_tab()
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ dash_app/app_content.py
@@ -0,0 +1,10 @@
+import dash_html_components as html
+
+def img_tab():
+    pass
+
+def model_tab():
+    pass
+
+def video_tab():
+    pass
